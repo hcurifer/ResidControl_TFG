dbce14b8819ce13589de10fff7ce4ff5dffa79e4
hcurifer <hcurifer@myuax.com>
2025-05-15 22:00:52 +0200

    Crear y programar componente menu
    Se crea y se programa el componente menu, se arranca para probar en dashboard, se importa el servicio para el buen funcionamiento de datepicker

--- Residcontrol/src/app/pages/login/page-login/page-login.component.ts
+++ Residcontrol/src/app/pages/login/page-login/page-login.component.ts
@@ -3,11 +3,14 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from '../../../components/sidebar/sidebar.component';
 import { NavbarComponent } from '../../../components/navbar/navbar.component';
 import { LoginComponent } from '../../../components/login/login.component';
+import { MenuComponent } from "../../../components/menu/menu.component";
 
 @Component({
   selector: 'app-page-login',
   templateUrl: './page-login.component.html',
   standalone: true,
-  imports: [SidebarComponent, NavbarComponent, LoginComponent]
+  imports: [SidebarComponent, NavbarComponent, LoginComponent, MenuComponent]
 })
-export class PageLoginComponent {}
+export class PageLoginComponent {
+  
+}
