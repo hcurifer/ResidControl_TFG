88dc5bf4278524f469af1611b2aeea36b87fa826
hcurifer <hcurifer@myuax.com>
2025-05-18 06:54:51 +0200

    actualizacion de navbar
    se mejora navbar, se termina y se borra extructura, se crean variables y funciones. se testea

--- Residcontrol/src/app/components/navbar/navbar.component.ts
+++ Residcontrol/src/app/components/navbar/navbar.component.ts
@@ -2,15 +2,30 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatToolbarModule, MatButtonModule],
    templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  
+
+  isLoggedIn = false; //  cambiar al integrar login real
+  userFullName = 'Nombre Apellido'; //  simulado, cambiar al conectar con el backend
+
+  constructor(private router: Router) {}
+
+  handleLoginLogout() {
+    if (this.isLoggedIn) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
 }
