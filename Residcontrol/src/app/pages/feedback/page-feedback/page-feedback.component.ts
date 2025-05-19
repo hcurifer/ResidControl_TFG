381affdb3198814658b02180015a98a4637e3b5e
hcurifer <hcurifer@myuax.com>
2025-05-19 19:11:47 +0200

    actualizacion
    Se realiza la estructura del html, el diseño del scss y se importa los modulos que necesitamos de typescrispt

--- Residcontrol/src/app/pages/feedback/page-feedback/page-feedback.component.ts
+++ Residcontrol/src/app/pages/feedback/page-feedback/page-feedback.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../../../components/menu/menu.component';
+import { ProgressCircleComponent } from '../../../components/ProgressCircle/progreso/progreso.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-page-feedback',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, MenuComponent, ProgressCircleComponent, MatButtonModule],
   templateUrl: './page-feedback.component.html',
   styleUrl: './page-feedback.component.scss'
 })
 export class PageFeedbackComponent {
-
+  
 }
