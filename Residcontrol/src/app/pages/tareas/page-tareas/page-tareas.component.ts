a697c1baffc6524419d60fbebbd8765fe457f6c9
hcurifer <hcurifer@myuax.com>
2025-05-19 15:48:42 +0200

    primera version de page tareas
    Se realiza la estructura de la pagina y se le da un diseño, se realiza los import de los componentes

--- Residcontrol/src/app/pages/tareas/page-tareas/page-tareas.component.ts
+++ Residcontrol/src/app/pages/tareas/page-tareas/page-tareas.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../../../components/menu/menu.component';
+import { TareasPendientesComponent } from '../../../components/tareas-pendientes/tareas-pendientes.component';
+import { TareasCompletadasComponent } from '../../../components/tareas-completadas/tareas-completadas.component';
 
 @Component({
   selector: 'app-page-tareas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MenuComponent, TareasPendientesComponent, TareasCompletadasComponent],
   templateUrl: './page-tareas.component.html',
   styleUrl: './page-tareas.component.scss'
 })
 export class PageTareasComponent {
-
+ 
 }
