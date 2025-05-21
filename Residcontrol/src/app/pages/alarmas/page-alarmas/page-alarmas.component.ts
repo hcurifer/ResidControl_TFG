4d830d4f648c1e9f38302051dc6d75022185f9e7
hcurifer <hcurifer@myuax.com>
2025-05-21 07:07:42 +0200

    actualizacion
    se retoca pagina y se crea simulacion para hacer tester

--- Residcontrol/src/app/pages/alarmas/page-alarmas/page-alarmas.component.ts
+++ Residcontrol/src/app/pages/alarmas/page-alarmas/page-alarmas.component.ts
@@ -3,14 +3,19 @@ import { AlertasComponent } from '../../../components/alertas/alerta/alerta.comp
 import { MenuComponent } from '../../../components/menu/menu.component';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { ProgressCircleComponent } from '../../../components/ProgressCircle/progreso/progreso.component';
 
 @Component({
   selector: 'app-page-alarmas',
   standalone: true,
-  imports: [CommonModule, MenuComponent, AlertasComponent, MatButtonModule],
+  imports: [CommonModule, MenuComponent, AlertasComponent, MatButtonModule, ProgressCircleComponent],
   templateUrl: './page-alarmas.component.html',
   styleUrl: './page-alarmas.component.scss'
 })
 export class PageAlarmasComponent {
-  totalAlertas: number = 3; 
+  totalAlertas = 8; 
+  completadas = 5;  
+  get porcentaje(): number {
+    return Math.round((this.completadas / this.totalAlertas) * 100);
+  }
 }
