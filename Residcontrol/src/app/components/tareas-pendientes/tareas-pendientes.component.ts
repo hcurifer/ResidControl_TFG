d4a01f966a75b29e8479b385d30c3971b05ad5e2
hcurifer <hcurifer@myuax.com>
2025-05-21 18:41:57 +0200

    Actualizacion
    Se modifica componentes para darle el estilo final y animaciones cuando sean usados

--- Residcontrol/src/app/components/tareas-pendientes/tareas-pendientes.component.ts
+++ Residcontrol/src/app/components/tareas-pendientes/tareas-pendientes.component.ts
@@ -1,14 +1,44 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-tareas-pendientes',
   standalone: true,
-  imports: [CommonModule, MatCheckboxModule],
+  imports: [CommonModule, MatCheckboxModule, NgFor, NgIf],
   templateUrl: './tareas-pendientes.component.html',
-  styleUrls: ['./tareas-pendientes.component.scss']
+  styleUrl: './tareas-pendientes.component.scss',
+   animations: [
+    trigger('tareaAnimada', [
+      state(
+        'pendiente',
+        style({ transform: 'translateX(0)', opacity: 1 })
+      ),
+      state(
+        'completada',
+        style({ transform: 'translateX(100px)', opacity: 0 })
+      ),
+      transition('pendiente => completada', [
+        animate('1000ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class TareasPendientesComponent {
   @Input() tareas: { descripcion: string; horas: number }[] = [];
+  tareasCompletadas: Set<string> = new Set();
+
+  completarTarea(tarea: { descripcion: string; horas: number }) {
+    this.tareasCompletadas.add(tarea.descripcion);
+
+    setTimeout(() => {
+      this.tareas = this.tareas.filter(t => t.descripcion !== tarea.descripcion);
+      this.tareasCompletadas.delete(tarea.descripcion);
+    }, 1500); 
+  }
+
+  estaCompletada(tarea: { descripcion: string }) {
+    return this.tareasCompletadas.has(tarea.descripcion);
+  }
 }
