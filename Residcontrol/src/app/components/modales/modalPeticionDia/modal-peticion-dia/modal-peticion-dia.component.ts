59794ca8007c75ee9b1b108e32d6fd0c3d34eebd
hcurifer <hcurifer@myuax.com>
2025-05-22 17:48:22 +0200

    Primer boton
    Se implementa el primer boton y se hace test de forma local

--- Residcontrol/src/app/components/modales/modalPeticionDia/modal-peticion-dia/modal-peticion-dia.component.ts
+++ Residcontrol/src/app/components/modales/modalPeticionDia/modal-peticion-dia/modal-peticion-dia.component.ts
@@ -7,6 +7,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-modal-peticion-dia',
@@ -18,7 +20,8 @@ import { FormsModule } from '@angular/forms';
     MatDatepickerModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    MatSnackBarModule
   ],
   templateUrl: './modal-peticion-dia.component.html',
   styleUrls: ['./modal-peticion-dia.component.scss']
@@ -28,8 +31,10 @@ export class ModalPeticionDiaComponent {
 
   constructor(
     public dialogRef: MatDialogRef<ModalPeticionDiaComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private snackBar: MatSnackBar
   ) {}
+
 
   cerrar() {
     this.dialogRef.close();
@@ -37,11 +42,16 @@ export class ModalPeticionDiaComponent {
 
   enviar() {
     const mensaje = `Solicitud de día para: ${this.fechaSeleccionada?.toLocaleDateString()}`;
-    console.log('ENVIAR CORREO:', {
-      to: this.data.to,
-      subject: 'Petición de día',
-      body: mensaje
+      console.log('ENVIAR CORREO:', {
+        to: this.data.to,
+        subject: 'Petición de día',
+        body: mensaje
     });
-    this.dialogRef.close();
+      this.dialogRef.close();
+      this.snackBar.open('Petición enviada correctamente', 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top'
+    });
   }
 }
