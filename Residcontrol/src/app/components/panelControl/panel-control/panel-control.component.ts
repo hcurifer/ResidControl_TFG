bbe3d86fccc44333ac3801d93e0a82af391d2a63
hcurifer <hcurifer@myuax.com>
2025-05-25 00:19:52 +0200

    implementacion
    implementacion del boton asignar Tarea

--- Residcontrol/src/app/components/panelControl/panel-control/panel-control.component.ts
+++ Residcontrol/src/app/components/panelControl/panel-control/panel-control.component.ts
@@ -4,11 +4,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { ModalCrearUsuarioComponent } from '../../modales/modalCrearUsuario/modal-crear-usuario/modal-crear-usuario.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ModalCrearResidenteComponent } from '../../modales/modalCrearResidente/modal-crear-residente/modal-crear-residente.component';
+import { ModalAsignarTareaComponent } from '../../modales/modalAsignarTareas/modal-asignar-tareas/modal-asignar-tareas.component';
 
 @Component({
   selector: 'app-panel-control',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, ModalCrearUsuarioComponent, MatDialogModule, ModalCrearResidenteComponent],
+  imports: [CommonModule,
+  MatButtonModule,
+  ModalCrearUsuarioComponent,
+  MatDialogModule,
+  ModalCrearResidenteComponent,
+  ModalAsignarTareaComponent
+  ],
   templateUrl: './panel-control.component.html',
   styleUrl: './panel-control.component.scss'
 })
@@ -33,5 +40,12 @@ export class PanelControlComponent {
     });
   }
 
+  abrirModalAsignarTarea() {
+    this.dialog.open(ModalAsignarTareaComponent, {
+      width: '480px',
+      disableClose: false,
+      hasBackdrop: true
+    });
+  }
 
 }
