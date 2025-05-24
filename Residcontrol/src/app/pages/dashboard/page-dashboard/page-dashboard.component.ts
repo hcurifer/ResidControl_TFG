e670a4c638c387ab786fc1562deb191273337030
hcurifer <hcurifer@myuax.com>
2025-05-24 22:28:11 +0200

    implementacion modal
    Se implenta modal en el componnte

--- Residcontrol/src/app/pages/dashboard/page-dashboard/page-dashboard.component.ts
+++ Residcontrol/src/app/pages/dashboard/page-dashboard/page-dashboard.component.ts
@@ -7,12 +7,24 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { PanelControlComponent } from '../../../components/panelControl/panel-control/panel-control.component';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 
 @Component({
   selector: 'app-page-dashboard',
   standalone: true,
-  imports: [CommonModule, MenuComponent, ResidenteCardComponent, UsuarioCardComponent, MatFormFieldModule, MatSelectModule, MatOptionModule, PanelControlComponent],
+  imports: [CommonModule,
+  MenuComponent,
+  ResidenteCardComponent,
+  UsuarioCardComponent,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  PanelControlComponent,
+  MatButtonModule,
+  MatDialogModule
+  ],
   templateUrl: './page-dashboard.component.html',
   styleUrl: './page-dashboard.component.scss'
 })
@@ -41,6 +53,5 @@ residentes = [
 ];
 
 residenteSeleccionado = this.residentes[0];
-
 
 }
